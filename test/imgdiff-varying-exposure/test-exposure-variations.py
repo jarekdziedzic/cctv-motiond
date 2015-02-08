00949f051ce11350117b5457f61e9938b47d0d7a
Jarek Dziedzic <jarek.dziedzic@gmail.com>
2015-02-08 13:20:58 +0000

    Added more failing tests.
    Interestingly the result of image comparison depends on the order of images.

--- test/imgdiff-varying-exposure/test-exposure-variations.py
+++ test/imgdiff-varying-exposure/test-exposure-variations.py
@@ -6,9 +6,12 @@ from subprocess import call
 
 def main():
     images = [ ["images/brighter.jpeg", "images/darker.jpeg"],
-               ["images/brighter2.jpeg", "images/darker2.jpeg"] ]
+               ["images/brighter2.jpeg", "images/darker2.jpeg"],
+               #["images/brighter3.jpeg", "images/darker3.jpeg"],
+               ["images/darker3.jpeg", "images/brighter3.jpeg"]]
 
     for img in images:
+        print "Comparing ", img
         rv = call(["../../imgdiff", img[0], img[1], "/tmp"])
 
         if rv == 0:
