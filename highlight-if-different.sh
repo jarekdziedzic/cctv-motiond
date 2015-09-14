da5c3503469922eebe95b7d758aa126fe197b863
Jarek Dziedzic <jarek.dziedzic@gmail.com>
2015-09-14 07:56:51 +0100

    Updated cctv-motiond to understand new way of returning result from imgdiff

--- highlight-if-different.sh
+++ highlight-if-different.sh
@@ -6,15 +6,16 @@ cropdir="0_ciekawe/wycinki"
 #imgdiff should do it
 mkdir -p "0_ciekawe/wycinki" &>/dev/null || true
 
-/usr/local/bin/imgdiff $1 $2 ${cropdir}
+result=`/usr/local/bin/imgdiff $1 $2 ${cropdir}`
 
 ec=$?
-if [ ${ec} -eq 66 ]; then
-    echo "Intruder found."
+if [ ${result} -ne "0" ]; then
+    echo "Intruder found. Diffrent pixel count = ${result}"
     echo "Highlighting ${1} and ${2} in $PWD"
     ln -s `readlink -f ${1}` "0_ciekawe/$(basename ${1})" || true
     ln -s `readlink -f ${2}` "0_ciekawe/$(basename ${2})" || true
 elif [ ${ec} -ne 0 ]; then
+    echo "Imgdiff error."
     exit ${ec}
 fi
 
